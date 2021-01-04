45977cbd9a2ed2c02696d340ec5d10ddae1f4e33
vishvakbandi <vishvakbandi@gmail.com>
2021-01-03 23:53:35 -0600

    home screen + API

--- src/screens/ScannerScreen.js
+++ src/screens/ScannerScreen.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 
-export default function App() {
+import discogsAPI from "../API/discogs";
+import config from "../../config";
+import axios from "axios";
+
+const ScannerScreen = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
@@ -16,7 +20,38 @@ export default function App() {
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+
+    getDataWithBarcode(data);
+    console.log("scanned");
   };
+
+  async function getDataWithBarcode(data) {
+    try {
+      /* const response = await discogsAPI.get(
+        "database/search",
+        {
+          params: {
+            barcode: data,
+          },
+        },
+        {
+          headers: {
+            Authorization: `Discogs key=${config.key}, secret=${config.secret}`,
+          },
+        }
+      );
+      */
+      const SIG = `&key=${config.key}&secret=${config.secret}`;
+      const requestURL = `https://api.discogs.com/database/search?barcode=${data}&key=xJibNNdNUNHWJZQrVBcx&secret=omPNJHWHoZmNlPvtbRjzuhtDyrIXqQAq`;
+
+      const response = await axios.get(requestURL);
+
+      console.log(response);
+      console.log("hi");
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
@@ -36,7 +71,7 @@ export default function App() {
       )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -46,3 +81,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+export default ScannerScreen;
