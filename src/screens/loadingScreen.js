c3b4fa74d163ed764b30dfa1c588e49b8cc4d1c5
vishvakbandi <vishvakbandi@gmail.com>
2021-01-09 17:54:58 -0600

    add loading screen

--- src/screens/loadingScreen.js
+++ src/screens/loadingScreen.js
@@ -1,49 +1,24 @@
 import React from "react";
-import { AcitvityIndicator, StyleSheet, Text, View } from "react-native";
-import * as SplashScreen from 'expo-splash-screen';
+import { render } from "react-dom";
+import { ActivityIndicator, Text, View } from "react-native";
 
-class LoadingScreen extends React.Component {
-  state = { appIsReady: false };
-  
-  async componentDidMount {
-    // Prevent native splash screen from autohiding
-    try {
-      await SplashScreen.preventAutoHideAsync();
-    } catch (e) {
-      console.warn(e);
+const LoadingScreen = () => {
+    render() {
+        return (
+            <View>
+                <ActivityIndicator
+                    animating={true}
+                    color="white"
+                    size="large"
+                    style={{margin: 15}}
+                />
+
+                <Text>
+                    {this.props.loadingText}
+                </Text>
+            </View>
+        )
     }
-    this.prepareResources();
-  }
-
-  prepareResources = async () => {
-    try {
-      await preformAPICalls();
-    } catch (e) {
-      console.log(e);
-    } finally {
-      this.setState({ appIsReady: true }), async = () => {
-        await SplashScreen.hideAsync();
-      }
-    }
-  }
-
-  return (
-    <View style={[styless.container, sstyles.horizontal]}>
-      <ActivityIndicator size="large" color="#0000ff" />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  horizontal: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    padding: 10,
-  },
-});
+}
 
 export default LoadingScreen;
