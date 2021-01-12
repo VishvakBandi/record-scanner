11cc56bf85d54f36fa02ef3b8583500b453208ba
vishvakbandi <vishvakbandi@gmail.com>
2021-01-11 23:52:00 -0600

    styling changes

--- src/screens/loadingScreen.js
+++ src/screens/loadingScreen.js
@@ -1,19 +1,24 @@
 import React from "react";
-import { ActivityIndicator, Text, View } from "react-native";
+import { ActivityIndicator, Text, View, StyleSheet } from "react-native";
 
 const LoadingScreen = (props) => {
   return (
-    <View>
-      <ActivityIndicator
-        animating={true}
-        color="white"
-        size="large"
-        style={{ margin: 15 }}
-      />
-
-      <Text>{props.loadingText}</Text>
+    <View style={[styles.containter, styles.horizontal]}>
+      <ActivityIndicator animating={true} color="black" size="large" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10,
+  },
+});
 
 export default LoadingScreen;
